2b28e51dd30edfcc1248ec26447329a4f5a997e0
Haileab <womahaileab1428@gmail.com>
2023-11-14 19:25:13 +0300

    changes

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,8 @@
 const NavBar = ()=>{
     return (
-       <header>
-        <div className="flex justify-between bg-slate-200 mx-auto">
-            <h1>
+       <header className="bg-slate-200">
+        <div className="flex justify-between max-w-6xl mx-auto p-3">
+            <h1 className="font-bold">
                 <span className="text-xl ">Sahand</span>
                 <span className="text-xl">Estate</span>
             </h1>
@@ -10,6 +10,7 @@ const NavBar = ()=>{
                 <input
                 type="text"
                 placeholder="Search ..."
+                className="rounded-lg"
                 />
             </form>
             <ul className="flex gap-2">
