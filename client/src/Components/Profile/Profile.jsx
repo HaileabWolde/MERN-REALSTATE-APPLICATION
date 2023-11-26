7aca7d27329eba8e72221ac1c01fd0b2ecc5ffb1
Haileab <womahaileab1428@gmail.com>
2023-11-26 10:15:03 +0300

    added profile details

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from "react-redux"
 const Profile = ()=>{
+    const { CurrentUser} =useSelector((state)=> state.user)
 return (
-    <div>
-        <h1>Profile</h1>
+    <div className="max-w-lg mx-auto">
+        <h1 className="font-bold text-3xl text-center mt-4">Profile</h1>
+        <img src={CurrentUser.avator} className="rounded-full h-24 w-24 object-cover self-center" alt="avator"/>
+        <form className="flex flex-col gap-2">
+        <input type="text" placeholder="username"/>
+        <input type="email" placeholder="email"/>
+        <input type="password" placeholder="Password"/>
+        </form>
+       
     </div>
 )
 }
