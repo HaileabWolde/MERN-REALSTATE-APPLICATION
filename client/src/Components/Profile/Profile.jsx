90baeaa5e1c4e7b2bb42fe7e2354b10e201feb7f
Haileab <womahaileab1428@gmail.com>
2023-11-26 21:21:38 +0300

    added some beauty to it

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react"
 import { useSelector } from "react-redux"
 const Profile = ()=>{
     const { CurrentUser} =useSelector((state)=> state.user)
+    const [formdata, setFormData] = useState({
+        Username: '',
+        email: '',
+        password: ''
+    })
+    const handleChange = (e)=>{
+        setFormData({
+            ...formdata,
+            [e.target.name]: e.target.value
+        })
+    }
+  
 return (
     <div className="max-w-lg mx-auto mt-14">
         <h1 className="font-semibold text-4xl text-center">Profile</h1>
-        <img src={CurrentUser.avator} className="rounded-full h-24 w-24 object-cover mx-auto my-6" alt="avator"/>
         <form className="flex flex-col gap-4">
+        <img src={CurrentUser.avator} className="rounded-full h-24 w-24 object-cover mx-auto my-6 cursor-pointer" alt="avator"/>
         <input type="text" 
         placeholder="Username"
-        value={CurrentUser.Username}
-        className="p-4 rounded-lg text-lg"/>
+        name="Username"
+        defaultValue={CurrentUser.Username}
+        onChange={handleChange}
+        className="p-4 border rounded-lg text-lg"/>
         <input type="email" 
-        placeholder="Email" 
-        className="p-4 rounded-lg text-lg"
-        value={CurrentUser.email}
+        placeholder="Email"
+        name="email" 
+        onChange={handleChange}
+        className="p-4  border rounded-lg text-lg"
+        defaultValue={CurrentUser.email}
         />
         <input type="password" 
-        placeholder="Password" 
-        className="p-4 rounded-lg text-lg"
-        value={CurrentUser.password}/>
-        <button type="submit" className="text-white bg-slate-700 p-4 rounded-lg text-xl">UPDATE</button>
+        placeholder="Password"
+        onChange={handleChange}
+        name="password" 
+        className="p-4 border rounded-lg text-lg"
+        defaultValue={CurrentUser.password}/>
+        <button type="submit" className="text-white bg-slate-700 p-4 rounded-lg text-xl hover:opacity-90">UPDATE</button>
         </form>
         <div className="flex justify-between mt-5">
             <span className="text-red-700 text-lg">Delete Account</span>
