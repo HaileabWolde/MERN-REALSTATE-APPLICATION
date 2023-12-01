38b5d6145cb3fb47f668aad75605ef29c5a160f7
Haileab <womahaileab1428@gmail.com>
2023-12-01 23:40:17 +0300

    added delete lisiting api route

--- server/controllers/listing.js
+++ server/controllers/listing.js
@@ -19,6 +19,26 @@ export const getListing = async(req, res, next)=>{
         next(error)
     }
 }
+export const getList = async(req, res, next)=>{
+    const {id} = req.params
+    try{
+        const List = await Listing.findById(id)
+        res.status(200).json(List)
+    }
+    catch(error){
+        next(error)
+    }
+}
+export const editListing = async(req, res, next)=>{
+    const {id} = req.params
+    try{
+        const editedList = await Listing.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(200).json(editedList)
+    }
+    catch(error){
+        next(error)
+    }
+}
 export const deleteListing = async(req, res, next)=>{
     const {id} = req.params
     try{
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createListing, getListing, deleteListing} from '../controllers/listing.js'
+import { createListing, getListing, deleteListing, editListing, getList} from '../controllers/listing.js'
 import { Auth } from '../middlewares/Auth.js'
 const router = express.Router()
 
 router.post('/create', Auth , createListing)
+router.get('/list/:id', Auth, getList)
 router.get('/getListing', Auth, getListing)
+router.post('/editListing/:id', Auth, editListing)
 router.delete('/deleteListing/:id', Auth, deleteListing)
 
 export default router;
