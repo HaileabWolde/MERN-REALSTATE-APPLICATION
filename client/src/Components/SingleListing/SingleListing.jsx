a91ae016cf0dbfb1246ba9718b3f37b15959d38f
Haileab <womahaileab1428@gmail.com>
2023-12-04 10:25:08 +0300

    just finsished the single listing ui

--- client/src/Components/SingleListing/SingleListing.jsx
+++ client/src/Components/SingleListing/SingleListing.jsx
@@ -62,8 +62,8 @@ return (
     }
     {
         !Loading && !error && formdata && (
-           <>
            <div>
+               <div>
            <Swiper navigation>
             {formdata.imageurl.map((url) => (
               <SwiperSlide key={url}>
@@ -78,12 +78,12 @@ return (
             ))}
           </Swiper>
            </div>
-           <div className="mt-7 flex flex-col">
-                  <div className="flex gap -2">
-                    <span>{formdata.name} - $</span>
+           <div className="mt-7 flex flex-col max-w-4xl mx-auto gap-4 pb-12">
+                  <div className="flex gap-2">
+                    <span className="text-3xl font-semibold">{formdata.name} - $</span>
                     {
-                      formdata.type === "Rent" ? <span className="ml-2">{formdata.RegularPrice} / month</span>:
-                      <span className="ml-2">{formdata.RegularPrice}</span>
+                      formdata.type === "Rent" ? <span className="ml-2 text-3xl">{formdata.RegularPrice} / month</span>:
+                      <span className="ml-2 text-3xl">{formdata.RegularPrice}</span>
                     }
                   </div>
                   <div className="flex">
@@ -91,29 +91,49 @@ return (
                     <span>{formdata.address}</span>
                   </div>
                   <div className="flex gap-4">
-                    <button className="text-white bg-red-900">{formdata.type === "Rent" ? 'For Rent': 'For Sale'}</button>
+                    <p className="text-white bg-red-900 text-center p-2 rounded-lg w-[180px] text-lg">{formdata.type === "Rent" ? 'For Rent': 'For Sale'}</p>
                     {
-                      formdata.Discounted && <button className="text-white bg-red-900">${formdata.Discounted} discount</button>
+                      formdata.Discounted && <p className="text-white bg-green-900 text-center p-2 rounded-lg w-[180px] text-lg">${formdata.Discounted} discount</p>
                     }
                   </div>
                   <div className="flex">
-                    <span>Description - </span>
-                    <p> {formdata.description}</p>
+                    <span className="font-semibold text-lg">Description - </span>
+                    <p className="ml-2 text-lg"> {formdata.description}</p>
                   </div>
-                  <div className="flex gap-4">
-                    <div className="flex gap-2">
-                    <IoBedSharp size={24} style={{ color: '#8B4513' }} />
-                    <span>{formdata.Beds} Beds</span>
-                    </div>
-                    <div className="flex gap-2">
-                    <TiToilet size={24} style={{ color: '#0074D9' }} />
-                    <span>{formdata.Baths} Baths</span>
-                    </div>
-                  </div>
+                  <ul className="flex gap-4 text-green-900 text-lg">
+                    <li className="flex gap-2">
+                    <FaBed size={32} />
+                    {
+                      formdata.Beds > 1 ? `${formdata.Beds} Beds` : `${formdata.Beds} Bed`
+                    }
+                    </li>
+                    <li className="flex gap-2">
+                    < FaBath size={24}/>
+                      {
+                        formdata.Baths  > 1 ? 
+                          `${formdata.Baths} Baths`
+                        : `${formdata.Baths} Bath`
+                      }
+                    </li>
+                    <li className="flex gap-2">
+                    < FaParking size={24}/>
+                     {
+                      formdata.Parking ? 'Parking Spot' : 'No Parking'
+                     }
+                    </li>
+                    <li className="flex gap-2">
+                    <FaChair size={24}/>
+                     {
+                      formdata.Furnished ? 'Furnished' : 'Not Furnished'
+                     }
+                    </li>
+                  </ul>
 
            </div>
            
-           </>
+           </div>
+          
+          
         )
     }
   </main>
