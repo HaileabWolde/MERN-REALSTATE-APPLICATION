6f305f9a9546d44b821e1eba9ba1dd083e7845c5
Haileab <womahaileab1428@gmail.com>
2023-12-06 08:52:13 +0300

    added some functionality

--- server/controllers/listing.js
+++ server/controllers/listing.js
@@ -19,6 +19,45 @@ export const getListing = async(req, res, next)=>{
         next(error)
     }
 }
+export const SearchListing = async(req,res,next)=> {
+    try{
+        let limit = parseInt(req.query.limit) || 9
+        const startIndex = parseInt(req.params.startIndex) || 0
+        const SearchTerm = req.query.SearchTerm || ''
+        let type = req.query.type
+        if (type === 'all' || type === undefined){
+            type= {$in : ['Rent', 'Sell']}
+        }
+        let Offer = req.query.Offer
+        if (Offer === 'false' || Offer === undefined){
+            Offer = {$in : [false, true]}
+        }
+        let Parking = req.query.Parking
+        if(Parking === 'false' || Parking === undefined){
+            Parking = {$in : [false, true]}
+        }
+        let  Furnished = req.query.Furnished
+        if(Furnished === 'false' || Furnished === undefined){
+            Furnished = {$in : [false, true]}
+        }
+        const sort = req.query.sort || 'created_at'
+        const order = req.query.order || 'desc'
+
+        const listedSearch = await Listing.find({
+            name: { $regex:SearchTerm, $options: 'i' },
+            type,
+            Offer,
+            Furnished,
+            Parking
+        }).sort({[sort]: order}).limit(limit).skip(startIndex)
+
+        res.status(200).json(listedSearch)
+ 
+    }
+    catch(error){
+        next(error)
+    }
+}
 export const getList = async(req, res, next)=>{
     const {id} = req.params
     try{
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createListing, getListing, deleteListing, editListing, getList} from '../controllers/listing.js'
+import { createListing, getListing, deleteListing, editListing, getList, SearchListing} from '../controllers/listing.js'
 import { Auth } from '../middlewares/Auth.js'
 const router = express.Router()
-
-router.post('/create', Auth , createListing)
-router.get('/list/:id', getList)
+router.get('/getBySearch', SearchListing)
 router.get('/getListing', Auth, getListing)
+router.get('/list/:id', getList)
+router.post('/create', Auth , createListing)
 router.post('/editListing/:id', Auth, editListing)
 router.delete('/deleteListing/:id', Auth, deleteListing)
 
